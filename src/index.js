f7f9e7161e0a22c7b1b6e1c11427bffa8904f6a3
Andrey <tulevand@gmail.com>
2020-11-03 14:26:54 +0300

    Собрал вторую страницу

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@
 import './js/forms/find_number.js'
 import './js/form_elements/date_dropdown.js'
 import './js/form_elements/dropdown_guest.js'
+import './js/forms/registration.js'
+import './js/form_elements/text_field.js'
 
 // SCSS
-//import './assets/scss/main.scss'
+import './assets/scss/form_elements/radio_button.scss'
 
 // CSS
+import './assets/css/pages/landing.css'
 import './assets/css/modules/style_registred.css'
 import './assets/css/modules/style_unregister.css'
 import './assets/css/modules/style_footer.css'
@@ -16,6 +19,12 @@ import './assets/css/forms/find_number.css'
 import './assets/css/form_elements/date_dropdown.css'
 import './assets/css/form_elements/dropdown_guest.css'
 import './assets/css/form_elements/button_long.css'
+import './assets/css/pages/registration.css'
+import './assets/css/forms/registration.css'
+import './assets/css/form_elements/text_field.css'
+import './assets/css/form_elements/toggle.css'
+import './assets/css/form_elements/button_white.css'
+
 
 // Vue.js
 //window.Vue = require('vue')
